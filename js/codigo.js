33a8b0a94609747fb9b2bc85bc885db17db08434
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-10-26 20:14:13 -0300

    Comienzo  a dar estructura de archivos, se crea archivo js que contiene el juego cachipun y es llamado desde el archivo html a traves de src

---
+++ js/codigo.js
@@ -0,0 +1,48 @@
+//Funcion para numero aleatorio del pc
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function eleccion(jugada){
+    let resultado = "";
+    if(jugada == 1){
+        resultado = "Piedra 🪨" ;
+    } else if(jugada == 2){
+        resultado = "Papel 📄";
+    } else if(jugada == 3){
+        resultado = "Tijeras ✂️";
+    } else{
+        resultado = "Elección inválida";
+    }
+    return resultado;
+}
+//1 es piedra, 2 es papel, 3 es tijera
+let jugador = 0
+
+//Invocacion de funcion
+let computador = 0
+victorias = 0
+derrotas = 0
+
+while (victorias < 3 && derrotas < 3) {
+    computador = aleatorio(1,3);
+            
+    jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
+
+    alert("Computadora elige " + eleccion(computador));
+    alert("Jugador elige " + eleccion(jugador));
+                                
+    //Victorias  y derrotas
+    if(jugador == computador){
+        alert("Empate!");
+    } else if(jugador == 1 && computador == 3 || jugador == 2 && computador == 1 || jugador == 3 && computador == 2){
+        alert("Ganaste!")
+        victorias = victorias + 1
+    } else{
+        alert("Perdiste!");
+        derrotas = derrotas + 1
+}
+}
+            
+alert("Ganaste " + victorias + " veces y perdiste " + derrotas + " veces.");
+                    
