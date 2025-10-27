50c9e622a287dfee24d3b55715212db0a146bdc7
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-10-26 23:01:51 -0300

    Se trabaja funcion queee nos devolverá cual es el espiritu que selecciona el jugador, se declaran variables por cada spiritu con su id del archivo html y luego en un if anidado se vee 1 por 1 si esta checkeado el espirutu seleccionado y se cierra con un else en caso de que no se haya seleccionado uno

--- rpg/js/rpg.js
+++ rpg/js/rpg.js
@@ -1,7 +1,42 @@
-function seleccionarEspirituJugador() {
-    alert("¡Has seleccionado a tu espíritu compañero!");
+function iniciarJuego() {
+    let botonMascota = document.getElementById('boton-espiritu')
+    botonMascota.addEventListener('click',  seleccionarEspirituJugador)
 }
 
-let botonMascota = document.getElementById('boton-espiritu')
+function seleccionarEspirituJugador() {
+    let inputSalamander = document.getElementById
+    ('salamander')
+    let inputUndine = document.getElementById('undine')
+    let inputSylph = document.getElementById('sylph')
+    let inputGnome = document.getElementById('gnome')
+    let inputGaia = document.getElementById('gaia')
+    let inputMagma = document.getElementById('magma')
+    let inputZephyr = document.getElementById('zephyr')
+    let inputNimbus = document.getElementById('nimbus')
 
-botonMascota.addEventListener('click', seleccionarEspirituJugador)
+    if (inputSalamander.checked){
+        alert('Has seleccionado a Salamander!')
+    } else if (inputUndine.checked){
+        alert('Has seleccionado a Undine!')
+    } else if (inputSylph.checked){
+        alert('Has seleccionado a Sylph!')
+    } else if (inputGnome.checked){
+        alert('Has seleccionado a Gnome!')
+    } else if (inputGaia.checked){
+        alert('Has seleccionado a Gaia!')
+    } else if (inputMagma.checked){
+        alert('Has seleccionado a Magma!')
+    } else if (inputZephyr.checked){
+        alert('Has seleccionado a Zephyr!')
+    } else if (inputNimbus.checked){
+        alert('Has seleccionado a Nimbus!')
+    }else{
+        alert('Selecciona un espíritu para continuar')
+    }    
+
+    
+}
+
+
+
+window.addEventListener('load', iniciarJuego)
