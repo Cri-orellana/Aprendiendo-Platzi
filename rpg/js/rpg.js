3e4ca86838b0c39dd1033cd116712474af522b50
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-10-26 22:27:47 -0300

    Inicio de incorporacion de javaScript, se comienza creando variable que tomara el espiritu seleccionado por el jugador utilizando document.getElemenById, el cual como dice su nombre selecciona el personaje por el id dado en el archivo html, y se crea eventListener que activara la funcion de seleccionarEspirituJugador al hacer click en el boton seleccionar

---
+++ rpg/js/rpg.js
@@ -0,0 +1,7 @@
+function seleccionarEspirituJugador() {
+    alert("¡Has seleccionado a tu espíritu compañero!");
+}
+
+let botonMascota = document.getElementById('boton-espiritu')
+
+botonMascota.addEventListener('click', seleccionarEspirituJugador)
